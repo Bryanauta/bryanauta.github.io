1c472d3e76a728ca5819a4e707e6959f3ba24057
Bryanauta <Bryanauta@hotmail.com>
2022-03-19 05:43:44 -0300

    v0.8

--- script.js
+++ script.js
@@ -26,8 +26,22 @@ listItem.forEach(element => {
     })
 })
 
-ScrollReveal().reveal('.home-section', { delay: 500 });
-ScrollReveal().reveal('.about-me-section', { delay: 500 });
-ScrollReveal().reveal('.skills-section', { delay: 500 });
-ScrollReveal().reveal('.projects-section', { delay: 500 });
-ScrollReveal().reveal('.contact-section', { delay: 500 });
+ScrollReveal().reveal(".slide-left", {
+    duration: 2000,
+    origin: "right",
+    distance: "1000px",
+    easing: "ease-in-out"
+});
+
+ScrollReveal().reveal(".slide-right", {
+    duration: 2000,
+    origin: "left",
+    distance: "1000px",
+    easing: "ease-in-out"
+});
+
+// ScrollReveal().reveal('.home-section', { delay: 500 });
+// ScrollReveal().reveal('.about-me-section', { delay: 500 });
+// ScrollReveal().reveal('.skills-section', { delay: 500 });
+// ScrollReveal().reveal('.projects-section', { delay: 500 });
+// ScrollReveal().reveal('.contact-section', { delay: 500 });
